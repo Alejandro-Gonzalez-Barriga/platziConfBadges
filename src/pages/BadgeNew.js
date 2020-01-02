6c42ffc581c347adaa3089fe2cf957230f6e0e7b
alexG <alejandro.gonzalez.barriga@gmail.com>
2020-01-02 16:20:59 -0600

    added handleSubmit to badgeNew

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -4,6 +4,7 @@ import "./styles/BadgeNew.css";
 import header from "../images/platziconf-logo.svg";
 import Badge from "../components/Badge";
 import BadgeForm from "../components/BadgeForm";
+import api from "../api";
 
 class BadgeNew extends React.Component {
   state = {
@@ -23,6 +24,18 @@ class BadgeNew extends React.Component {
         [e.target.name]: e.target.value
       }
     });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    this.setState({ loading: true, error: null });
+
+    try {
+      await api.badges.create(this.state.form);
+      this.setState({ loading: false });
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
   };
 
   render() {
