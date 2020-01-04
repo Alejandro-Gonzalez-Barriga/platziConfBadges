03c9eb16feb51656afe6eb66a3f11706d1852b44
alexG <alejandro.gonzalez.barriga@gmail.com>
2020-01-03 18:39:34 -0600

    added logo and styles to BadgeDetails.js

--- src/pages/BadgeDetails.js
+++ src/pages/BadgeDetails.js
@@ -1,8 +1,24 @@
 import React from "react";
 
+import "./styles/BadgeDetails.css";
+import confLogo from "../images/platziconf-logo.svg";
+
 class BadgeDetails extends React.Component {
   render() {
-    return "details";
+    return (
+      <div>
+        <div className="BadgeDetails__hero">
+          <div className="container">
+            <div className="row">
+              <div className="col-6">
+                <img src={confLogo} alt="conf logo" />
+              </div>
+              <div className="col-6"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
