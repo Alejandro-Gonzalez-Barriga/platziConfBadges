204f4bf2657aa46591b0fc6e53b5a7cf8b75c530
alexG <alejandro.gonzalez.barriga@gmail.com>
2020-01-06 17:46:41 -0600

    Updates

---
+++ precache-manifest.ffb5417c0110d6a39ad344f936ff1df7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/platziConfBadges/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "2c9fff6871f8a7b540d1",
+    "url": "/platziConfBadges/static/css/main.61dd8954.chunk.css"
+  },
+  {
+    "revision": "f44462464c8172c4f77d",
+    "url": "/platziConfBadges/static/js/runtime~main.f4446246.js"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/platziConfBadges/static/media/stars.0abcdfcd.svg"
+  },
+  {
+    "revision": "e7790bd8394c6756ebfe",
+    "url": "/platziConfBadges/static/js/2.e7790bd8.chunk.js"
+  },
+  {
+    "revision": "2c9fff6871f8a7b540d1",
+    "url": "/platziConfBadges/static/js/main.2c9fff68.chunk.js"
+  },
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/platziConfBadges/static/media/badge-header.ed7c58ea.svg"
+  },
+  {
+    "revision": "6841730f1bbe3235e7a0cdc7d244d244",
+    "url": "/platziConfBadges/static/media/logo.6841730f.svg"
+  },
+  {
+    "revision": "0d2ff286deb3895d3ee82fed78c4dce4",
+    "url": "/platziConfBadges/static/media/astronauts.0d2ff286.svg"
+  },
+  {
+    "revision": "e7790bd8394c6756ebfe",
+    "url": "/platziConfBadges/static/css/2.18478c48.chunk.css"
+  },
+  {
+    "revision": "3135bf4ed7688bfb67509853038882d6",
+    "url": "/platziConfBadges/index.html"
+  }
+];
